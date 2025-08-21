7e16a4b7a560211e066e89a2c7ca5748278bee3b
Stawowcz <jstawowcz@gmail.com>
2025-08-21 20:16:34 +0700

    changed percentage of workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 7,
+  workers: process.env.CI ? "100%" : "50%",
   reporter: "html",
   use: {
     baseURL:
