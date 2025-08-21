0ae8cedd3b9d4e6d84a626a07232ea7df4fcf712
Stawowcz <jstawowcz@gmail.com>
2025-08-21 20:17:27 +0700

    uncommented browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,15 +22,15 @@ export default defineConfig({
   },
 
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
 
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
 
     {
       name: "webkit",
